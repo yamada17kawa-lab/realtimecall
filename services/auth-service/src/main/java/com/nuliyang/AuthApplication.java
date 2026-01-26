65149dd2ec5515600670b90d8b109e6dcc33705a
yamada17 <yamada17kawa@gmail.com>
2026-01-26 22:12:27 +0800

    添加登录、注册、修改用户信息接口。auth-service服务调用user-service服务进行增查操作，达成登录、注册功能。user-service服务自己实现用户修改功能。对于新增、修改功能，会有事务操作，避免数据库数据脏乱。

--- services/auth-service/src/main/java/com/nuliyang/AuthApplication.java
+++ services/auth-service/src/main/java/com/nuliyang/AuthApplication.java
@@ -1,13 +1,19 @@
 package com.nuliyang;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@Slf4j
+@EnableFeignClients(basePackages = "com.nuliyang.feign")
 public class AuthApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AuthApplication.class, args);
+        log.info("认证服务启动成功……");
     }
 }
--- services/auth-service/src/main/java/com/nuliyang/service/AuthService.java
+++ services/auth-service/src/main/java/com/nuliyang/service/AuthService.java
@@ -1,4 +1,19 @@
 package com.nuliyang.service;
 
+import com.nuliyang.dto.LoginDto;
+import com.nuliyang.dto.RegisterDto;
+import com.nuliyang.vo.UserVo;
+
 public interface AuthService {
+
+    /**
+     * 注册
+     */
+    UserVo register(RegisterDto registerDto);
+
+
+    /**
+     * 登录
+     */
+    UserVo login(LoginDto loginDto);
 }
